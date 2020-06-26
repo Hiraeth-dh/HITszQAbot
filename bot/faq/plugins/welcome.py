fbab9bac2cf01a0042cd1d2bc9ea752f78b02406
L.M.Sherlock <l.m.kagamigawachloe@gmail.com>
2020-06-26 19:50:51 +0800

    更新欢迎词

--- bot/faq/plugins/welcome.py
+++ bot/faq/plugins/welcome.py
@@ -7,6 +7,6 @@ from .txt_tools import add_at
 async def _(session: NoticeSession):
     # 发送欢迎消息
     if str(session.ctx.get('group_id')) in config.while_list:
-        reply = '欢迎新朋友～，我是哈工深问答机器人小哈，欢迎向我提问哦！\n提问示例：“小哈 怎么填志愿才能上哈深？”'
+        reply = '欢迎新朋友～，我是哈工深问答机器人小哈，欢迎向我提问哦！\n提问示例：小哈 怎么填志愿才能上哈深？'
         reply = add_at(reply, session.ctx.get('user_id'))
         await session.send(reply)
