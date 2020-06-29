399165ec9b1b7cab17a86bfc22a70a7c6f08ae84
L.M.Sherlock <l.m.kagamigawachloe@gmail.com>
2020-06-29 20:35:17 +0800

    Update welcome.py

--- bot/faq/plugins/welcome.py
+++ bot/faq/plugins/welcome.py
@@ -7,6 +7,6 @@ from .txt_tools import add_at
 async def _(session: NoticeSession):
     # 发送欢迎消息
     if str(session.ctx.get('group_id')) in config.while_list:
-        reply = '欢迎新朋友～，我是哈工深问答机器人小哈，欢迎向我提问哦！\n提问示例：小哈 怎么填志愿才能上哈深？'
+        reply = '为了能更好的给您提供招生咨询服务，请您先规范群名片格式：高考省份简称+高考年份+F(父)/M(母)+网名+B/G考生男孩女孩，谢谢您的配合。\n改完群名片，您可以先看看群文件，群相册，有问题的可以提问，群里有招生老师、哈深学子、家长和问答机器人为您答疑。\n我是小哈，在问题开头加上“小哈”即可向我提问哦~'
         reply = add_at(reply, session.ctx.get('user_id'))
         await session.send(reply)
